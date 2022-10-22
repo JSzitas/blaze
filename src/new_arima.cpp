5b0e5268732f51d35177107b76a48ac4a199528e
JSzitas <szitas.juraj13@gmail.com>
2022-10-22 02:37:22 +0200

    Continue dev

--- src/new_arima.cpp
+++ src/new_arima.cpp
@@ -2,6 +2,7 @@
 using namespace Rcpp;
 #include "cmath"
 #include "utils.h"
+#include "poly.h"
 
 // originally partrans
 // [[Rcpp::export]]
@@ -116,30 +117,107 @@ bool ar_check( std::vector<double> & ar_coef, double tol = 0.0000001 ) {
   // goes here instead of the false
   // then the abs(/inverse/roots) must be all greater than > 1 for
   // this to lie within the unit circle
-  return false;
+
+  std::vector<double> coef(p+1);
+  coef[0] = 1;
+  for( int i = 1; i< coef.size(); i++ ) {
+    coef[i] = -ar_coef[i-1];
+  }
+  auto res = polyroot(coef);
+  // try to reuse a bit of memory :)
+  // coef.resize(res.size());
+  for( int i = 0; i < res.size(); i++ ) {
+    if( abs(res[i]) <= 1 ) {
+      // the all(Mod(x) > 1) part
+      // if any modulus from inverse coefficients is smaller than 1,
+      // that implies that the coefficient lies outside the unit circle
+      // and the process is not stationary
+      return false;
+    }
+  }
+  // otherwise all coefficients are ok >> return true
+  return true;
 }
 
-
-
-maInvert <- function(ma) {
-  q <- length(ma)
-  q0 <- max(which(c(1, ma) != 0)) - 1L
-  if (!q0)
-    return(ma)
-    roots <- polyroot(c(1, ma[1L:q0]))
-    ind <- Mod(roots) < 1
-  if (all(!ind))
-    return(ma)
-    if (q0 == 1)
-      return(c(1/ma[1L], rep.int(0, q - q0)))
-      roots[ind] <- 1/roots[ind]
-    x <- 1
-    for (r in roots) x <- c(x, 0) - c(0, x)/r
-      c(Re(x[-1L]), rep.int(0, q - q0))
+std::complex<double> operator /( double left, std::complex<double> right ) {
+  // perform the operation (ac+bd)/(c2 + d2) + (bc-ad)/(c2 + d2)i.
+  // left = a+bi
+  const double &a = left;
+  const double &c = std::real(right);
+  const double &d = std::imag(right);
+  // the formula simplifies to (ac)/(c^2 + d^2) + (-ad)/(c^2 + d^2)i
+  std::complex<double> res(
+      // real part
+      (left * c)/(pow(c,2) + pow(d,2)),
+      // complex part
+      (-a*d)/(pow(c,2) + pow(d,2))
+      );
+  return res;
 }
 
+std::vector<double> ma_invert( std::vector<double> & ma_coef, double tol = 0.0000001 ) {
+  auto q = ma_coef.size();
+  // analogous to ar_check >> find latest/largest order nonzero coef
+  int q0 = 0;
+  for( int i =0; i < ma_coef.size(); i++) {
+    if( abs(ma_coef[i]) > tol  ) {
+      q0 = i;
+    }
+  }
+  // if all coefs zero
+  if(!q0) {
+    return ma_coef;
+  }
+  // otherwise find roots
+  std::vector<double> coef(q0+1);
+  coef[0] = 1;
+  for( int i = 1; i< coef.size(); i++ ) {
+    coef[i] = ma_coef[i-1];
+  }
+  auto res = polyroot(coef);
+  // find roots that are smaller than 1 - these are not inverse roots,
+  // so they should be smaller, rather than larger than 1
+  std::vector<bool> indices(res.size());
+  int any = 0;
+  bool temp = false;
+  for( int i = 0; i < res.size(); i++ ) {
+    temp = abs(res[i]) < 1;
+    indices[i] = temp;
+    // this simplifies the all(!ind) part
+    any += temp;
+  }
+  // if all are > 1, we do not need to change them
+  // as this implies they are valid inverse roots already - and there is no
+  // need to invert
+  if(!any) {
+    return ma_coef;
+  }
+  // otherwise if there is only 1 valid coefficient
+  if( q0 == 1 ) {
+    coef.resize(q);
+    // only invert the very first coefficient and otherwise set other coefficients
+    // to zero
+    coef[0] = 1/ma_coef[0];
+    for( int i = 1; i < coef.size(); i++) {
+      coef[i] = 0;
+    }
+    return coef;
+  }
 
-
-
-
+  // otherwise invert the roots which need to be inverted
+  for( int i = 0; i < res.size(); i++ ) {
+    // I made this work for complex numbers :)
+    if( indices[i] ) {
+      res[i] = 1/res[i];
+    }
+  }
+  double x = 1;
+  for( auto &root:res ) {
+    // somewhat ugly
+    // x <- c(x, 0) - c(0,x)/r
+  }
+  // finally take the real parts
+  // c(Re(x[-1L]), rep.int(0, q - q0))
+  // and return
+}
 
