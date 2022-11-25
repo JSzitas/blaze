602ccf0c4fe174ef214e1567fe5391e0465dd703
Juraj Szitas <szitas.juraj13@gmail.com>
2022-11-25 01:16:18 +0100

    Update headers with dependencies

---
+++ src/eigen.hpp
@@ -0,0 +1,6 @@
+#ifndef EIGEN_INCLUDE
+#define EIGEN_INCLUDE
+
+#include "../inst/include/Eigen/Core"
+
+#endif
---
+++ src/optim.hpp
@@ -0,0 +1,6 @@
+#ifndef OPTIMLIB_INCLUDE
+#define OPTIMLIB_INCLUDE
+
+#include "../inst/include/optim/optim.hpp"
+
+#endif
