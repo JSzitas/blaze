6b2eb2137f8d7b651667bbb819ad67d1ba7dd12d
Juraj Szitas <szitas.juraj13@gmail.com>
2023-01-23 01:40:26 +0100

    Update forecast debug script

--- inst/fcst_debug.R
+++ inst/fcst_debug.R
@@ -9,19 +9,21 @@ D <- 0
 Q <- 0
 season <- 1
 
-arima_obj <- new( BlazeArima, c(lynx)[1:100], c(p,d,q,P,D,Q,season), list(), "Gardner", c(TRUE,TRUE), 1000000  )
+arima_obj <- new( BlazeArima, c(lynx)[1:100], c(p,d,q,P,D,Q,season),
+                  list(), "Gardner", c(TRUE,TRUE), 1000000  )
 arima_obj$fit()
+arima_obj$get_coef()
 
-arima_obj$forecast(14, list()) -> cpp_fcst
-
-arima_mod <- arima(c(lynx)[1:100], order = c(p,d,q), seasonal = list( order = c(P,D,Q), period = season),
-                   include.mean = TRUE,
-                   transform.pars = TRUE, kappa = 1000000, method = "CSS")
-predict(arima_mod, 14) -> r_fcst
-
-plot.ts(lynx[101:114])
-lines(cpp_fcst$forecast, col = "red")
-lines(c(r_fcst$pred), col = "green")
+# arima_obj$forecast(14, list()) -> cpp_fcst
+#
+# arima_mod <- arima(c(lynx)[1:100], order = c(p,d,q), seasonal = list( order = c(P,D,Q), period = season),
+#                    include.mean = TRUE,
+#                    transform.pars = TRUE, kappa = 1000000, method = "CSS")
+# predict(arima_mod, 14) -> r_fcst
+#
+# plot.ts(lynx[101:114])
+# lines(cpp_fcst$forecast, col = "red")
+# lines(c(r_fcst$pred), col = "green")
 
 
 
