8ae8c78a51818cb7122f63dcc7f514c955d0bb49
Juraj Szitas <szitas.juraj13@gmail.com>
2023-01-30 00:42:45 +0100

    Update forecast test to seasonal model

--- inst/fcst_debug.R
+++ inst/fcst_debug.R
@@ -2,20 +2,21 @@ remove(list=ls())
 pkgload::load_all(compile = TRUE)
 
 p <- 2
-d <- 0
+d <- 1
 q <- 1
-P <- 0
-D <- 0
-Q <- 0
-season <- 1
+P <- 1
+D <- 1
+Q <- 1
+season <- 5
+use_mean <- TRUE
 
-arima_obj <- new( BlazeArima, c(lynx)[1:100], c(p,d,q,P,D,Q,season), list(), "Gardner", c(TRUE,TRUE), 1000000  )
+arima_obj <- new( BlazeArima, c(lynx)[1:100], c(p,d,q,P,D,Q,season), list(), "Gardner", c(use_mean,TRUE), 1000000  )
 arima_obj$fit()
 
 arima_obj$forecast(14, list()) -> cpp_fcst
 
 arima_mod <- arima(c(lynx)[1:100], order = c(p,d,q), seasonal = list( order = c(P,D,Q), period = season),
-                   include.mean = TRUE,
+                   include.mean = use_mean,
                    transform.pars = TRUE, kappa = 1000000, method = "CSS")
 predict(arima_mod, 14) -> r_fcst
 
