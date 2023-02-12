2c9b596a83f6eba220d9ac37248e112be95f01a1
Juraj Szitas <szitas.juraj13@gmail.com>
2023-02-12 02:14:52 +0100

    Add better likelihood computation and fix ml solver

--- src/arima/solvers/arima_css_likelihood.h
+++ src/arima/solvers/arima_css_likelihood.h
@@ -149,183 +149,124 @@ template <const size_t update_point = 0,
     }
 }
 
-template <class T> double arima_likelihood(const T &y,
-                                           structural_model<double> &model) {
-  // define integers needed for further processing - these are mostly used
-  // for indexing and offsetting
+template <const size_t update_point = 0,
+          typename T> std::array<double,2> arima_likelihood(
+              const T &y,
+              structural_model<double> &model) {
   const size_t n = y.size(), rd = model.a.size(), p = model.phi.size(),
-      q = model.theta.size(), d = model.delta.size(), r = rd - d;
-  size_t nu = 0;
+    q = model.theta.size(), d = model.delta.size(), r = rd - d;
 
-  // define data structures needed for computation intermediaries
   std::vector<double> anew(rd);
   std::vector<double> M(rd);
-  std::vector<double> Pnew = model.Pn;
-  // this is only needed if we have any deltas
-  std::vector<double> mm( rd * rd * (d > 0) );
+  std::vector<double> mm( (d > 0) * rd * rd);
+  double ssq = 0, sumlog = 0;
+  size_t nu = y.size();
 
-  double tmp, vi, resid, gain, sumlog = 0, ssq = 0;
   for (size_t l = 0; l < n; l++) {
     for (size_t i = 0; i < r; i++) {
-      tmp = (i < r - 1) ? model.a[i + 1] : 0.0;
-      if (i < p) {
-        tmp += model.phi[i] * model.a[0];
-      }
+      double tmp = (i < r - 1) ? model.a[i + 1] : 0.0;
+      if (i < p) tmp += model.phi[i] * model.a[0];
       anew[i] = tmp;
     }
     if (d > 0) {
-      for (size_t i = r + 1; i < rd; i++) {
-        anew[i] = model.a[i - 1];
-      }
-      tmp = model.a[0];
-      for (size_t i = 0; i < d; i++) {
-        tmp += model.delta[i] * model.a[r + i];
-      }
+      for (size_t i = r + 1; i < rd; i++) anew[i] = model.a[i - 1];
+      double tmp = model.a[0];
+      for (size_t i = 0; i < d; i++) tmp += model.delta[i] * model.a[r + i];
       anew[r] = tmp;
     }
-    // only if we are past the first observation
-    if (l > 0) {
-      // if we have any thetas
+    if(l > update_point) {
       if (d == 0) {
         for (size_t i = 0; i < r; i++) {
-          vi = 0.0;
-          // presumably leading coefficient
-          if (i == 0) {
-            vi = 1.0;
-          } else if (i - 1 < q) {
-            vi = model.theta[i - 1];
-          }
+          double vi = 0.0;
+          if (i == 0) vi = 1.0; else if (i - 1 < q) vi = model.theta[i - 1];
           for (size_t j = 0; j < r; j++) {
-            tmp = 0.0;
-            if (j == 0) {
-              tmp = vi;
-            } else if (j - 1 < q) {
-              tmp = vi * model.theta[j - 1];
-            }
-            if (i < p && j < p) {
-              tmp += model.phi[i] * model.phi[j] * model.P[0];
-            }
-            if (i < r - 1 && j < r - 1) {
-              tmp += model.P[i + 1 + r * (j + 1)];
-            }
-            if (i < p && j < r - 1) {
-              tmp += model.phi[i] * model.P[j + 1];
-            }
-            if (j < p && i < r - 1) {
-              tmp += model.phi[j] * model.P[i + 1];
-            }
-            // update new P matrix with appropriate entry
-            Pnew[i + r * j] = tmp;
+            double tmp = 0.0;
+            if (j == 0) tmp = vi; else if (j - 1 < q) tmp = vi * model.theta[j - 1];
+            if (i < p && j < p) tmp += model.phi[i] * model.phi[j] * model.P[0];
+            if (i < r - 1 && j < r - 1) tmp += model.P[i + 1 + r * (j + 1)];
+            if (i < p && j < r - 1) tmp += model.phi[i] * model.P[j + 1];
+            if (j < p && i < r - 1) tmp += model.phi[j] * model.P[i + 1];
+            model.Pn[i + r * j] = tmp;
           }
         }
       } else {
         /* mm = TP */
-        for (size_t i = 0; i < r; i++) {
+        for (size_t i = 0; i < r; i++)
           for (size_t j = 0; j < rd; j++) {
-            tmp = 0.0;
-            if (i < p) {
-              tmp += model.phi[i] * model.P[rd * j];
-            }
-            if (i < r - 1) {
-              tmp += model.P[i + 1 + rd * j];
-            }
+            double tmp = 0.0;
+            if (i < p) tmp += model.phi[i] * model.P[rd * j];
+            if (i < r - 1) tmp += model.P[i + 1 + rd * j];
             mm[i + rd * j] = tmp;
           }
-        }
-        for (size_t j = 0; j < rd; j++) {
-          tmp = model.P[rd * j];
-          for (size_t k = 0; k < d; k++) {
-            tmp += model.delta[k] * model.P[r + k + rd * j];
-          }
-          mm[r + rd * j] = tmp;
-        }
-        for (size_t i = 1; i < d; i++) {
           for (size_t j = 0; j < rd; j++) {
-            mm[r + i + rd * j] = model.P[r + i - 1 + rd * j];
+            double tmp = model.P[rd * j];
+            for (size_t k = 0; k < d; k++)
+              tmp += model.delta[k] * model.P[r + k + rd * j];
+            mm[r + rd * j] = tmp;
           }
-        }
+          for (size_t i = 1; i < d; i++)
+            for (size_t j = 0; j < rd; j++)
+              mm[r + i + rd * j] = model.P[r + i - 1 + rd * j];
+
         /* Pnew = mmT' */
-        for (size_t i = 0; i < r; i++) {
+        for (size_t i = 0; i < r; i++)
           for (size_t j = 0; j < rd; j++) {
-            tmp = 0.0;
-            if (i < p) {
-              tmp += model.phi[i] * mm[j];
-            }
-            if (i < r - 1) {
-              tmp += mm[rd * (i + 1) + j];
-            }
-            Pnew[j + rd * i] = tmp;
+            double tmp = 0.0;
+            if (i < p) tmp += model.phi[i] * mm[j];
+            if (i < r - 1) tmp += mm[rd * (i + 1) + j];
+            model.Pn[j + rd * i] = tmp;
           }
-        }
-        for (size_t j = 0; j < rd; j++) {
-          tmp = mm[j];
-          for (size_t k = 0; k < d; k++) {
-            tmp += model.delta[k] * mm[rd * (r + k) + j];
-          }
-          Pnew[rd * r + j] = tmp;
-        }
-        for (size_t i = 1; i < d; i++) {
           for (size_t j = 0; j < rd; j++) {
-            Pnew[rd * (r + i) + j] = mm[rd * (r + i - 1) + j];
+            double tmp = mm[j];
+            for (size_t k = 0; k < d; k++)
+              tmp += model.delta[k] * mm[rd * (r + k) + j];
+            model.Pn[rd * r + j] = tmp;
           }
-        }
+          for (size_t i = 1; i < d; i++)
+            for (size_t j = 0; j < rd; j++)
+              model.Pn[rd * (r + i) + j] = mm[rd * (r + i - 1) + j];
         /* Pnew <- Pnew + (1 theta) %o% (1 theta) */
         for (size_t i = 0; i <= q; i++) {
-          vi = (i == 0) ? 1. : model.theta[i - 1];
-          for (size_t j = 0; j <= q; j++) {
-            Pnew[i + rd * j] += vi * ((j == 0) ? 1. : model.theta[j - 1]);
-          }
+          double vi = (i == 0) ? 1. : model.theta[i - 1];
+          for (size_t j = 0; j <= q; j++)
+            model.Pn[i + rd * j] += vi * ((j == 0) ? 1. : model.theta[j - 1]);
         }
       }
     }
     if (!isnan(y[l])) {
-      resid = y[l] - anew[0];
-      for (size_t i = 0; i < d; i++) {
+      double resid = y[l] - anew[0];
+      for (size_t i = 0; i < d; i++)
         resid -= model.delta[i] * anew[r + i];
-      }
+
       for (size_t i = 0; i < rd; i++) {
-        tmp = Pnew[i];
-        for (size_t j = 0; j < d; j++) {
-          tmp += Pnew[i + (r + j) * rd] * model.delta[j];
-        }
+        double tmp = model.Pn[i];
+        for (size_t j = 0; j < d; j++)
+          tmp += model.Pn[i + (r + j) * rd] * model.delta[j];
         M[i] = tmp;
       }
-      gain = M[0];
-      for (size_t j = 0; j < d; j++) {
-        gain += model.delta[j] * M[r + j];
-      }
-      // if gain is reasonable, update nu, residual sum of squares and
-      // sum of log gain
+
+      double gain = M[0];
+      for (size_t j = 0; j < d; j++) gain += model.delta[j] * M[r + j];
       if (gain < 1e4) {
-        nu++;
         ssq += resid * resid / gain;
         sumlog += log(gain);
+      } else {
+        nu--;
       }
-      // you would normally update the residuals here
-      // also, you get to update a, and P - this should change them by
-      // reference (so that you do not have to return them)
-      for (size_t i = 0; i < rd; i++) {
+      for (size_t i = 0; i < rd; i++)
         model.a[i] = anew[i] + M[i] * resid / gain;
-      }
-      for (size_t i = 0; i < rd; i++) {
-        for (size_t j = 0; j < rd; j++) {
-          model.P[i + j * rd] = Pnew[i + j * rd] - M[i] * M[j] / gain;
-        }
-      }
+      for (size_t i = 0; i < rd; i++)
+        for (size_t j = 0; j < rd; j++)
+          model.P[i + j * rd] = model.Pn[i + j * rd] - M[i] * M[j] / gain;
     } else {
-      // model updates
-      for (size_t i = 0; i < rd; i++) {
-        model.a[i] = anew[i];
-      }
-      for (size_t i = 0; i < rd * rd; i++) {
-        model.P[i] = Pnew[i];
-      }
-      // if you were updating residuals, here you would put in an 'NA' or NaN
+      for (size_t i = 0; i < rd; i++) model.a[i] = anew[i];
+      for (size_t i = 0; i < rd * rd; i++) model.P[i] = model.Pn[i];
     }
   }
   // finally, compute likelihood and return
-  auto s2 = ssq/(double)nu;
-  return 0.5 * log(s2) + (sumlog/(double)nu);
+  const double s2 = ssq/(double)nu;
+  const double loglik = 0.5 * (log(s2) + (sumlog/(double)nu));
+  return std::array<double,2>{s2, loglik};
 }
 
 #endif
--- src/arima/solvers/arima_ml_solver.h
+++ src/arima/solvers/arima_ml_solver.h
@@ -34,6 +34,7 @@ private:
   EigVec y_temp, new_x;
   std::vector<double> residual, temp_phi, temp_theta;
   structural_model<double> model;
+  double sigma2;
 public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
   // initialize with a given arima structure
@@ -61,15 +62,16 @@ public:
     this->temp_theta = std::vector<double>(kind.q() + (kind.Q() * kind.period()));
     if constexpr(seasonal || transform) {
       // the expansion of arima parameters is only necessary for seasonal models
-      arima_transform_parameters<seasonal, transform>(
+      arima_transform_parameters<EigVec, seasonal, transform>(
           this->new_x, this->kind, this->temp_phi, this->temp_theta
       );
     }
     // // initialize state space model
     this->model = make_arima( this->new_x, delta, this->kind, kappa, ss_init);
+    this->sigma2 = 0;
   }
   double operator()(const Eigen::VectorXd &x) {
-    for (int i = 0; i < x.size(); i++) this->new_x(i) = x(i);
+    for (size_t i = 0; i < x.size(); i++) this->new_x(i) = x(i);
     if constexpr (has_xreg) {
       // refresh y_temp and load it with original y data
       for (size_t i = 0; i < this->n; i++) this->y_temp(i) = this->y[i];
@@ -81,7 +83,7 @@ public:
      */
     if constexpr(seasonal || transform) {
       // the expansion of arima parameters is only necessary for seasonal models
-      arima_transform_parameters<seasonal, transform>(
+      arima_transform_parameters<EigVec, seasonal, transform>(
           this->new_x, this->kind, this->temp_phi, this->temp_theta
       );
     }
@@ -89,11 +91,17 @@ public:
     update_arima(this->model, this->new_x, this->kind, this->ss_init);
     // return likelihood - check if this updated model or not (it ideally
     // should not, not here)
-    return arima_likelihood(this->y_temp, this->model);
+    const std::array<double,2> res = arima_likelihood(this->y_temp, this->model);
+    this->sigma2 = res[0];
+    return res[1];
   }
-  structural_model<double> get_structural_model() const {
-    return this->model;
+  void finalize() {
+    // recompute  and replace V as this has not been updated
+    // since initialization in make_arima
+    recompute_v(this->model);
   }
+  double get_sigma() const { return this->sigma2; }
+  structural_model<double> get_structural_model() const { return this->model; }
 };
 
 template <const bool has_xreg, const bool seasonal, const bool transform>
@@ -106,14 +114,13 @@ void arima_solver_ml(std::vector<double> &y,
                      std::vector<double> &delta, const double kappa,
                      const SSinit ss_init, double &sigma2) {
 
-  auto vec_size = kind.p() + kind.q() + kind.P() + kind.Q() + xreg_coef.size();
-  auto arma_size = kind.p() + kind.q() + kind.P() + kind.Q();
+  const auto vec_size = coef.size();
+  const auto arma_size = kind.p() + kind.q() + kind.P() + kind.Q();
   Eigen::VectorXd x(vec_size);
-  for (size_t i = 0; i < coef.size(); i++) x(i) = coef[i];
+  for (size_t i = 0; i < vec_size; i++) x(i) = coef[i];
   // initialize to all zeroes except for xreg
-  for (size_t i = arma_size; i < vec_size; i++) {
-    x[i] = xreg_coef.coef[i - arma_size];
-  }
+  for (size_t i = arma_size;
+       i < vec_size; i++) x[i] = xreg_coef.coef[i - arma_size];
   // initialize solver
   cppoptlib::solver::Bfgs<
     ARIMA_ML_PROBLEM<has_xreg, seasonal, transform>> solver;
@@ -122,12 +129,15 @@ void arima_solver_ml(std::vector<double> &y,
       y, kind, xreg_coef.has_intercept(), xreg, delta, kappa, ss_init);
   // and finally, minimize
   auto [solution, solver_state] = solver.Minimize(ml_arima_problem, x);
-  // update variance estimate for the arima model - this was passed by reference
-  sigma2 = exp(2 * solution.value);
+  // replace model V
+  ml_arima_problem.finalize();
+  // update variance estimate for the arima model
+  sigma2 = ml_arima_problem.get_sigma();
   // pass fitted coefficients back to the caller
   for (size_t i = 0; i < vec_size; i++) coef[i] = solution.x[i];
   // write back structural model
   model = ml_arima_problem.get_structural_model();
+  for (size_t i = 0; i < vec_size; i++) coef[i] = solution.x[i];
 }
 
 #endif
