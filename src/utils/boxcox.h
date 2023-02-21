998ec3a718e2f2cf8d52bf69d6f7d610ae2763af
Juraj Szitas <szitas.juraj13@gmail.com>
2023-02-21 01:04:08 +0100

    Minor improvements to box_cox

--- src/utils/boxcox.h
+++ src/utils/boxcox.h
@@ -3,25 +3,20 @@
 
 #include <cmath>
 
-template <typename T> T small_max(T a, T b) {
-  if (a > b) {
-    return a;
-  }
-  return b;
-}
 
-template <typename NumKind = double>
-NumKind lambda_coef(std::vector<NumKind> &x, NumKind lambda, int period = 2) {
+
+template <typename scalar_t = double>
+scalar_t lambda_coef(std::vector<scalar_t> &x, scalar_t lambda, const size_t period = 2) {
   // pre-compute the number of windows to compute over
   // this is integer division so the if the last period does not exactly overlap
   // it should only contain however many items are available
-  int num_buckets = ceil((float)x.size() / period);
+  const size_t num_buckets = ceil((float)x.size() / period);
 
-  std::vector<NumKind> mu_ests(num_buckets);
-  std::vector<NumKind> sigma_ests(num_buckets);
+  std::vector<scalar_t> mu_ests(num_buckets);
+  std::vector<scalar_t> sigma_ests(num_buckets);
   // split data into buckets defined by period and compute averages
-  for (int i = 0; i < num_buckets; i++) {
-    for (int j = 0; j < period; j++) {
+  for (size_t i = 0; i < num_buckets; i++) {
+    for (size_t j = 0; j < period; j++) {
       // i indexes the seasonal period - the bucket
       // j index step within seasonal period
       // sum within the the estimate
@@ -31,8 +26,8 @@ NumKind lambda_coef(std::vector<NumKind> &x, NumKind lambda, int period = 2) {
     mu_ests[i] = mu_ests[i] / period;
   }
   // now compute the standard errors
-  for (int i = 0; i < num_buckets; i++) {
-    for (int j = 0; j < period; j++) {
+  for (size_t i = 0; i < num_buckets; i++) {
+    for (size_t j = 0; j < period; j++) {
       // i indexes the seasonal period - the bucket
       // j index step within seasonal period
       // sum the squares within the estimate
@@ -40,41 +35,37 @@ NumKind lambda_coef(std::vector<NumKind> &x, NumKind lambda, int period = 2) {
     }
     // now divide this by the number of seasonal periods - 1 ( the N-1 formula
     // for variance ) (optionally 1 in case we only have a period of 1)
-    sigma_ests[i] = sigma_ests[i] / small_max(period - 1, 1);
+    sigma_ests[i] = sigma_ests[i] / std::max(period - 1, (size_t)1);
     // finally we need to take the square root of this
     sigma_ests[i] = sqrt(sigma_ests[i]);
   }
   // now compute the ratios
-  for (int i = 0; i < num_buckets; i++) {
+  for (size_t i = 0; i < num_buckets; i++) {
     // we can very happy reuse the mu_ests without having to allocate more
     // memory
     mu_ests[i] = sigma_ests[i] / pow(mu_ests[i], 1.0 - lambda);
   }
   // compute standard deviation divided by the mean
-  NumKind final_mu = 0.0, final_sigma = 0.0;
-  for (int i = 0; i < num_buckets; i++) {
+  scalar_t final_mu = 0.0, final_sigma = 0.0;
+  for (size_t i = 0; i < num_buckets; i++) {
     final_mu = final_mu + mu_ests[i];
     final_sigma = final_sigma + pow(mu_ests[i], 2);
   }
-  final_mu = final_mu / (NumKind)num_buckets;
-  final_sigma = final_sigma / (NumKind)num_buckets;
-  // // subtract mean
+  final_mu = final_mu / (scalar_t)num_buckets;
+  final_sigma = final_sigma / (scalar_t)num_buckets;
+  // subtract mean
   final_sigma = final_sigma - pow(final_mu, 2);
   final_sigma = sqrt(final_sigma);
   return final_sigma / final_mu;
 }
 
-template <typename T> struct box_cox_obj_wrap {
-  box_cox_obj_wrap(std::vector<T> &data, T par) : x(data), par(par) {}
-  T operator()(T par_val) {
-    // when you call this with par, it provides an evaluation of par
-    // using the objective and the data. I am sure this can be turned to
-    // a variable argument function with template metaprogramming...
-    // but that misses the point
+template <typename scalar_t> struct box_cox_obj_wrap {
+  box_cox_obj_wrap(std::vector<scalar_t> &data, scalar_t par) : x(data), par(par) {}
+  scalar_t operator()(scalar_t par_val) {
     return lambda_coef(x, par_val);
   }
-  std::vector<T> &x;
-  T par;
+  std::vector<scalar_t> &x;
+  scalar_t par;
 };
 
 #endif
