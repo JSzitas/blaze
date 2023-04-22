62a15d1f67750d6819da025609f97bd982c89bbb
Juraj Szitas <szitas.juraj13@gmail.com>
2023-04-23 00:06:37 +0200

    Add better way to include all structures at once

--- src/arima/structures/fitting_method.h
+++ src/arima/structures/fitting_method.h
@@ -1,6 +1,6 @@
 #ifndef FITTING_METHOD
 #define FITTING_METHOD
 
-enum fitting_method { CSS = 1, CSSML = 2, ML = 3 };
+enum fitting_method { CSS = 1, CSSML = 2, ML = 3, LASSO = 4, HUBER = 5 };
 
 #endif
---
+++ src/arima/structures/structures.h
@@ -0,0 +1,9 @@
+#ifndef STRUCTURES
+#define STRUCTURES
+
+#include "arima/structures/ss_init.h"
+#include "arima/structures/fitting_method.h"
+#include "arima/structures/arima_kind.h"
+#include "arima/structures/structural_model.h"
+
+#endif
