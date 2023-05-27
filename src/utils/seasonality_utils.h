b6418057a88182ef235aed4cb8a682b529ba9f4b
Juraj Szitas <szitas.juraj13@gmail.com>
2023-05-27 02:13:52 +0200

    Finalize seasonality identification utils

--- src/utils/seasonality_utils.h
+++ src/utils/seasonality_utils.h
@@ -4,44 +4,36 @@
 #include "utils/utils.h"
 #include "auto_ar/auto_ar.h"
 
-#include <algorithm>
-
 template <typename scalar_t> size_t period(
-    const std::vector<scalar_t>&y, const scalar_t threshold = 10.0,
+    const std::vector<scalar_t>&y, const scalar_t threshold = 10,
     const size_t n_frequencies = 500) {
-  
-  const size_t n = y.size(); 
-  const size_t max_p = min(n - 1L, std::floor(10 * std::log10(n)));
-  
+  const size_t n = y.size(), max_p = min(n - 1L, std::floor(10 * std::log10(n)));
   auto res = AutoAR(y, 1, max_p,
                     std::vector<std::vector<scalar_t>>(0, std::vector<scalar_t>(0)),
-                    true, false, true, AutoARMethod::AIC);
-  
+                    true, false, true, AutoARMethod::AICc);
+  // do not forget to fit the model
+  res.fit();
   auto freq = regular_sequence<scalar_t>(0.0, 0.5, n_frequencies);
   const size_t order = res.get_order();
   const scalar_t var_p = res.get_sigma2();
   auto coef = res.get_coef();
 
-  constexpr scalar_t two_pi = 6.283185;
-  
+  static constexpr scalar_t two_pi = 6.283185;
   std::vector<scalar_t> spec(n_frequencies, var_p);
   if(order >= 1) {
     for(size_t i = 0; i < n_frequencies; i++) {
       scalar_t temp_cos = 0.0, temp_sin = 0.0; 
       for( size_t j = 0; j < order; j++ ) {
-        scalar_t temp = two_pi * freq[i] * order;
+        scalar_t temp = two_pi * freq[i] * (j+1);
         temp_cos += std::cos(temp) * coef[j];
         temp_sin += std::sin(temp) * coef[j];
       }
       spec[i] = var_p/(std::pow(1 - temp_cos, 2) + std::pow(temp_sin,2));
     }
   }
-  size_t period = 1;
-  
+  size_t period = 1, max_spec_index = 0, j = 0;
   scalar_t max_spec = 0.0;
-  size_t max_spec_index = 0;
-  size_t j = 0;
-  for( auto&item:spec) {
+  for(auto&item:spec) {
     if(item > max_spec) {
       max_spec = item;
       max_spec_index = j;
@@ -50,10 +42,10 @@ template <typename scalar_t> size_t period(
   }
   if(max_spec > threshold)
   {
-    period = std::round(1/freq[max_spec_index]);
+    period = std::round(1.0/freq[max_spec_index]);
     // Find next local maximum
-    if(std::isinf(period)) {
-      auto diff_spec_positive = diff(spec);
+    if(std::isinf(period) || max_spec_index == 0) {
+      std::vector<scalar_t> diff_spec_positive = diff(spec);
       std::vector<size_t> positive_indices;
       for(size_t j = 0; j < diff_spec_positive.size(); j++) {
         if(diff_spec_positive[j] > 0) {
@@ -62,12 +54,14 @@ template <typename scalar_t> size_t period(
       }
       if(positive_indices.size() > 0)
       {
-        size_t nextmax = positive_indices[0] + max_at(spec, positive_indices[1], spec.size());
+        // index in difference is '1 earlier' since our diff produces output of
+        // size input-differences
+        size_t nextmax = 1 + max_at(spec, positive_indices[0], spec.size());
         period = std::round(1/freq[nextmax]);
       }
     }
   }
-  return static_cast<size_t>(period); 
+  return period;
 } 
 
 template <typename scalar_t> std::vector<scalar_t> windowed_sum(
@@ -88,10 +82,9 @@ template <typename scalar_t> std::vector<size_t> find_seasonalities(
   const size_t max_iter = 5,
   const size_t upper_limit = 1500 ) {
   std::vector<size_t> periods;
-  
   for(size_t j = 0; j < max_iter; j++) {
     size_t last_period = period(y);
-    if( last_period <= 1 ){
+    if(last_period <= 1 || (last_period > y.size())){
       break;
     }
     periods.push_back(last_period);
